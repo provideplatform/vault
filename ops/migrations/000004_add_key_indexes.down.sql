375eb66e10cb84fde0024ed893e7f57492a8ecdf
Kyle Thomas <kyle@provide.services>
2020-09-05 06:59:00 -0400

    Drop type index from secrets in latest migration

--- ops/migrations/000004_add_key_indexes.down.sql
+++ ops/migrations/000004_add_key_indexes.down.sql
@@ -1,6 +1,5 @@
 DROP INDEX idx_keys_iteration;
 DROP INDEX idx_keys_spec;
 DROP INDEX idx_keys_usage;
-DROP INDEX idx_secrets_type;
 
 CREATE INDEX idx_keys_iteration ON keys USING hash (iteration);
--- ops/migrations/000004_add_key_indexes.up.sql
+++ ops/migrations/000004_add_key_indexes.up.sql
@@ -3,5 +3,3 @@ CREATE INDEX idx_keys_iteration ON keys USING btree (iteration);
 
 CREATE INDEX idx_keys_spec ON keys USING btree (spec);
 CREATE INDEX idx_keys_usage ON keys USING btree (usage);
-
-CREATE INDEX idx_secrets_type ON secrets USING btree (type);

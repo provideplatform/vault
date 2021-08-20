abdf0adf1a68881164e1f5cd89b83b1cad18742f
Elliot <elliotharris@live.co.uk>
2021-08-20 16:35:01 +0100

    Don't use root to docker push

--- ops/ecs_deploy.sh
+++ ops/ecs_deploy.sh
@@ -71,7 +71,7 @@ docker_build()
 
     echo 'Docker push...'
     aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
-    sudo docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/provide/vault:${buildRef}"
+    docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/provide/vault:${buildRef}"
 }
 
 ecs_deploy()

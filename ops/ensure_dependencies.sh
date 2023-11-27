76e3ec8f6fe54bc308595b634f654877f0c34d70
kt <kyle@provide.services>
2023-11-27 14:50:48 -0500

    Remove commented code

--- ops/ensure_dependencies.sh
+++ ops/ensure_dependencies.sh
@@ -7,8 +7,6 @@ fi
 
 if [ ! -d "./vendor/github.com/herumi/bls-eth-go-binary/bls/include" ] ; then
   git clone -b release https://github.com/herumi/bls-eth-go-binary "${GOPATH}/src/github.com/herumi/bls-eth-go-binary"
-#   mkdir -p "./vendor/github.com/herumi/bls-eth-go-binary/bls/include"
-#   mkdir -p "./vendor/github.com/herumi/bls-eth-go-binary/bls/lib"
   cp -r "${GOPATH}/src/github.com/herumi/bls-eth-go-binary/bls/include" "./vendor/github.com/herumi/bls-eth-go-binary/bls"
-    cp -r "${GOPATH}/src/github.com/herumi/bls-eth-go-binary/bls/lib" "./vendor/github.com/herumi/bls-eth-go-binary/bls"
+  cp -r "${GOPATH}/src/github.com/herumi/bls-eth-go-binary/bls/lib" "./vendor/github.com/herumi/bls-eth-go-binary/bls"
 fi
